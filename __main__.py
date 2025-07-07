f8203a2529e659e2251a2cae5a7b4f0fdd7f713a
Elina Tugusheva <tugusheva.elina@mail.ru>
2025-07-07 20:59:23 +0500

    Task 3

--- __main__.py
+++ __main__.py
@@ -8,13 +8,34 @@ class Student:
         self.grades = {}
 
     def rate_lecture(self, lecturer, course, grade):
-        if isinstance(lecturer, Lecturer) and course in lecturer.courses_attached and course in self.courses_in_progress:
+        if isinstance(lecturer,
+                      Lecturer) and course in lecturer.courses_attached and course in self.courses_in_progress:
             if course in lecturer.grades:
                 lecturer.grades[course] += [grade]
             else:
                 lecturer.grades[course] = [grade]
         else:
             return 'Ошибка'
+
+    def add_grade(self, grade):
+        if isinstance(grade, float) and 0 <= grade <= 10:
+            self.grades.append(grade)
+
+    @property
+    def average_grade(self):
+        return sum(self.grades) / len(self.grades) if self.grades else None
+
+    def __str__(self):
+        avg_grade = f"{self.average_grade:.1f}" if self.average_grade is not None else 'Нет оценок'
+        return f'Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за домашние задания: {avg_grade}\nКурсы в процессе изучения: {", ".join(self.courses_in_progress)}\nЗавершенные курсы: {", ".join(self.finished_courses)}'
+
+    def __eq__(self, other):
+        if isinstance(other, Student):
+            return self.average_grade == other.average_grade
+
+    def __lt__(self, other):
+        if isinstance(other, Student):
+            return self.average_grade < other.average_grade
 
 
 class Mentor:
@@ -30,6 +51,27 @@ class Lecturer(Mentor):
         self.courses_attached = []
         self.courses_in_progress = []
         self.grades = {}
+
+    def add_grade(self, grade):
+        if isinstance(grade, float) and 0 <= grade <= 10:
+            self.grades.append(grade)
+
+    @property
+    def average_grade(self):
+        return sum(self.grades) / len(self.grades) if self.grades else None
+
+    def __str__(self):
+        avg_grade = f"{self.average_grade:.1f}" if self.average_grade is not None else 'Нет оценок'
+        return f'Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за лекции: {avg_grade}'
+
+    def __eq__(self, other):
+        if isinstance(other, Lecturer):
+            return self.average_grade == other.average_grade
+
+    def __lt__(self, other):
+        if isinstance(other, Lecturer):
+            return self.average_grade < other.average_grade
+
 
 class Reviewer(Mentor):
     def __init__(self, name, surname):
@@ -47,18 +89,22 @@ class Reviewer(Mentor):
         else:
             return 'Ошибка'
 
+    def __str__(self):
+        return f'Имя: {self.name}\nФамилия: {self.surname}'
 
-lecturer = Lecturer('Иван', 'Иванов')
-reviewer = Reviewer('Пётр', 'Петров')
-student = Student('Алёхина', 'Ольга', 'Ж')
 
-student.courses_in_progress += ['Python', 'Java']
-lecturer.courses_attached += ['Python', 'C++']
-reviewer.courses_attached += ['Python', 'C++']
+some_reviewer = Reviewer('Some', 'Buddy')
+some_lecturer = Lecturer('Some', 'Buddy')
+some_lecturer.grades = {9.9, 10}
 
-print(student.rate_lecture(lecturer, 'Python', 7))  # None
-print(student.rate_lecture(lecturer, 'Java', 8))  # Ошибка
-print(student.rate_lecture(lecturer, 'С++', 8))  # Ошибка
-print(student.rate_lecture(reviewer, 'Python', 6))  # Ошибка
+some_student = Student('Ruoy', 'Eman', 'Male')
+some_student.grades = {9.9, 10}
+some_student.courses_in_progress = ['Python', 'Git']
+some_student.finished_courses = ['Введение в программирование']
 
-print(lecturer.grades)  # {'Python': [7]}
+print('Проверяющий:  ')
+print(some_reviewer)
+print('Лектор:  ')
+print(some_lecturer)
+print('Студент:  ')
+print(some_student)
